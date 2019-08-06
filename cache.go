2c75717f659cfd039c141c1f972f58c58f2f3c9f
申法宽 <shenfakuan@163.com>
2019-08-06 14:48:51 +0800

    修改

--- cache.go
+++ cache.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"crypto/md5"
+	"encoding/json"
 	"fmt"
 	"github.com/go-redis/redis"
 	"time"
@@ -13,9 +14,25 @@ type Client struct {
 	tags       []string
 }
 
+// RedisDriver RedisDriver
+var RedisDriver *redis.Client
+
 // NewClient NewClient
 func NewClient() *Client {
-	InitRedis()
+	client := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "sdfsdf", // no password set
+		DB:       0,        // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("redis pong: ", pong)
+
+	RedisDriver = client
 
 	return &Client{
 		baseClient: RedisDriver,
@@ -30,33 +47,55 @@ func (c *Client) Tag(tag ...string) *Client {
 
 // Put .Tag().Put()
 func (c *Client) Put(key string, val interface{}, expire int64) error {
-	InitRedis()
-
 	for _, v := range c.tags {
-		err := RedisDriver.SAdd(key, fmt.Sprintf("tag:%v", fmt.Sprintf("%x", md5.Sum([]byte(v))))).Err()
-
+		err := RedisDriver.SAdd(fmt.Sprintf("tag:%v", fmt.Sprintf("%x", md5.Sum([]byte(v)))), key).Err()
 		if err != nil {
 			fmt.Println("err:", err)
 		}
 	}
 
-	err := RedisDriver.Set(key, val, time.Hour).Err()
+	value, err := json.Marshal(val)
+	if err != nil {
+		fmt.Println("err:", err)
+		return err
+	}
+
+	err = RedisDriver.Set(fmt.Sprintf("key:%v", key), string(value), time.Hour).Err()
 
 	if err != nil {
 		fmt.Println("err:", err)
+		return err
 	}
 
 	return nil
 }
 
 // Clear .Tag().Clear()
-func (c *Client) Clear(key string) error {
-	InitRedis()
+func (c *Client) Clear() error {
+	for _, val := range c.tags {
+		// get key
+		key := fmt.Sprintf("tag:%v", fmt.Sprintf("%x", md5.Sum([]byte(val))))
 
-	err := RedisDriver.Del(key).Err()
+		// get members
+		members, err := RedisDriver.SMembers(key).Result()
+		if err != nil {
+			fmt.Println("err:", err)
+			return err
+		}
 
-	if err != nil {
-		fmt.Println("err:", err)
+		// delete members
+		err = RedisDriver.Del(members...).Err()
+		if err != nil {
+			fmt.Println("err:", err)
+			return err
+		}
+
+		// delete tag
+		err = RedisDriver.Del(key).Err()
+		if err != nil {
+			fmt.Println("err:", err)
+			return err
+		}
 	}
 	return nil
 }
